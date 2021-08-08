eab77e3647930e170121d18502aec10b828ba04d
Jeremias Moreira Gomes <j3r3miasmg@gmail.com>
2021-08-08 18:00:18 -0300

    New covid files in 08-08-2021-18-00.

--- covid/brazil-vaccination-stats-per-state.py
+++ covid/brazil-vaccination-stats-per-state.py
@@ -15,7 +15,7 @@ UFnames = {
         'Maranhão', 'MT': 'Mato Grosso', 'MS': 'Mato Grosso do Sul', 'MG':
         'Minas Gerais', 'PA': 'Pará', 'PB': 'Paraíba', 'PR': 'Paraná', 'PE':
         'Pernambuco', 'PI': 'Piauí', 'RJ': 'Rio de Janeiro', 
-        'RN': 'Rio Grande do Norte', 'RS': 'Riop Grande do Sul', 
+        'RN': 'Rio Grande do Norte', 'RS': 'Rio Grande do Sul', 
         'RO': 'Rondônia', 'RR': 'Roraima', 'SC': 'Santa Catarina', 
         'SP': 'São Paulo', 'SE': 'Sergipe', 'TO': 'Tocantins', 
         'DF': 'Federal District', 'TOTAL': 'Brazil'
